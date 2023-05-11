a5131e0d78e744f7468e7d87d6bc006e93fcba59
April Zhang <aprilzh23@gmail.com>
2023-05-11 14:17:40 -0700

    Resolve code review requests

--- tests/merge_sorted_lists_test.py
+++ tests/merge_sorted_lists_test.py
@@ -4,11 +4,11 @@ from functions.merge_sorted_lists import merge_sorted_lists
 
 def test_for_1_and_2():
     # Arrange
-    l1 = [1]
-    l2 = [2]
+    list1 = [1]
+    list2 = [2]
 
     # Act
-    answer = merge_sorted_lists(l1, l2)
+    answer = merge_sorted_lists(list1, list2)
 
     # Assert
     assert answer == [1, 2]
@@ -16,23 +16,23 @@ def test_for_1_and_2():
 
 def test_for_2_and_1():
     # Arrange
-    l1 = [2]
-    l2 = [1]
+    list1 = [2]
+    list2 = [1]
 
     # Act
-    answer = merge_sorted_lists(l1, l2)
+    answer = merge_sorted_lists(list1, list2)
 
     # Assert
     assert answer == [1, 2]
 
 
-def test_for_1_2_3_and_4_5_6():
+def test_for_merge_two_list():
     # Arrange
-    l1 = [1, 2, 3]
-    l2 = [4, 5, 6]
+    list1 = [1, 2, 3]
+    list2 = [4, 5, 6]
 
     # Act
-    answer = merge_sorted_lists(l1, l2)
+    answer = merge_sorted_lists(list1, list2)
 
     # Assert
     assert answer == [1, 2, 3, 4, 5, 6]
